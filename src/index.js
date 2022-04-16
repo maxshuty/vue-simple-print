13ed3368590db7721d927e152347df92441bcce0
Max Poshusta <MaxPoshusta@gmail.com>
2022-04-16 07:03:56 -0500

    Adding style function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function addExistingStyles(iframeContentWindow) {
   // Querying the existing document for all possible stylesheets and adding them to the iframe
   const styleSheets = Array.from(
+    document.querySelectorAll('link[rel="stylesheet"]')
+  ).map((link) => link.href);
 }
 
 
